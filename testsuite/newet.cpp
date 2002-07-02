24f3b3eb1ba3cd436352e12e619bfc7e1d5b54ce
jcumming <devnull@localhost>
2002-07-02 19:42:48 +0000

    Added testing of functorExpr.h header file, which provides support for declaring user-defined functors and class methods that act on Array types. The code for this support originated from Derrick Bass of Caltech.

--- testsuite/newet.cpp
+++ testsuite/newet.cpp
@@ -1,9 +1,39 @@
 #define BZ_NEW_EXPRESSION_TEMPLATES
 
 #include "testsuite.h"
-#include <blitz/Array.h>
+#include <blitz/array.h>
 
 BZ_USING_NAMESPACE(blitz)
+
+double myFunction(double x)
+{
+    return 1.0 / (1.0 + x);
+}
+
+BZ_DECLARE_FUNCTION(myFunction)
+
+class myFunctor
+{
+public:
+    myFunctor(double p) : period_(p) { }
+    double operator()(double x) const
+    { return sin(x/period_); }
+    BZ_DECLARE_FUNCTOR(myFunctor)
+private:
+    double period_;
+};
+
+class myClass
+{
+public:
+    myClass(double p) : power_(p) { }
+    int foo(double x, double y) const
+    { return int(x * pow(y,power_)); }
+    BZ_DECLARE_MEMBER_FUNCTION2_RET(myClass,foo,int)
+private:
+    double power_;
+};
+
 
 int main()
 {
@@ -30,5 +60,29 @@ int main()
 
     A = tensor::i;
     BZTEST(count(A==C)==4);
+
+    Array<double,1> E(4), F(4);
+    E = 2.0 * sqrt(cast<double>(tensor::i * tensor::i));
+    F = 2.0 * tensor::i;
+    BZTEST(count(E==F)==4);
+
+    E = sqr(tensor::i + 1.0);
+    F = pow(tensor::i+1.0,2.0);
+    BZTEST(count(E==F)==4);
+
+    E = 4.0 * tensor::i;
+    F = myFunction(E);
+    E = 1.0 / (1.0 + 4.0 * tensor::i);
+    BZTEST(count(E==F)==4);
+
+    myFunctor f1(10.0);
+    E = f1(2.5 + tensor::i);
+    F = sin((2.5 + tensor::i)/10.0);
+    BZTEST(count(E==F)==4);
+
+    myClass f2(3.5);
+    A = f2.foo(1.2 + tensor::i, 1.2 - tensor::i);
+    B = cast<int>((1.2 + tensor::i) * pow(1.2 - tensor::i, 3.5));
+    BZTEST(count(A==B)==4);
 }
 
