ee46fdf6f171de5e1ca145cc4e1af370c675cd92
tveldhui <devnull@localhost>
2002-10-03 14:41:13 +0000

    *** empty log message ***

--- testsuite/chris-jeffery-3.cpp
+++ testsuite/chris-jeffery-3.cpp
@@ -13,7 +13,9 @@ test = 5;
 Range I(2,6) ;
 Range J(3,7) ;
 
-test2 = where(test(I,J) > test(I-1,J), 0, test(I,J));
+// Next line should be test(I,J) > test(I-1,J) but this causes a
+// conflict with std::relops; is there a less ugly workaround?
+test2 = where(blitz::operator> (test(I,J), test(I-1,J)), 0, test(I,J));
 
 BZTEST(test2(3,3) == 5);
 
