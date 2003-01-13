1b29aaa0f8368e37a5b06159dfd5b00d2c52105a
patricg <devnull@localhost>
2003-01-13 10:08:37 +0000

    Removed the cvs log and added them to the ChangeLog

--- random/mt.h
+++ random/mt.h
@@ -1,53 +1,45 @@
-/* A C-program for MT19937: Integer version (1998/4/6)            */
-/*  genrand() generates one pseudorandom unsigned integer (32bit) */
-/* which is uniformly distributed among 0 to 2^32-1  for each     */
-/* call. sgenrand(seed) set initial values to the working area    */
-/* of 624 words. Before genrand(), sgenrand(seed) must be         */
-/* called once. (seed is any 32-bit integer except for 0).        */
-/*   Coded by Takuji Nishimura, considering the suggestions by    */
-/* Topher Cooper and Marc Rieffel in July-Aug. 1997.              */
-
-/* This library is free software; you can redistribute it and/or   */
-/* modify it under the terms of the GNU Library General Public     */
-/* License as published by the Free Software Foundation; either    */
-/* version 2 of the License, or (at your option) any later         */
-/* version.                                                        */
-/* This library is distributed in the hope that it will be useful, */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of  */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.            */
-/* See the GNU Library General Public License for more details.    */
-/* You should have received a copy of the GNU Library General      */
-/* Public License along with this library; if not, write to the    */
-/* Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA   */ 
-/* 02111-1307  USA                                                 */
-
-/* Copyright (C) 1997 Makoto Matsumoto and Takuji Nishimura.       */
-/* When you use this, send an email to: matumoto@math.keio.ac.jp   */
-/* with an appropriate reference to your work.                     */
-
-/* REFERENCE                                                       */
-/* M. Matsumoto and T. Nishimura,                                  */
-/* "Mersenne Twister: A 623-Dimensionally Equidistributed Uniform  */
-/* Pseudo-Random Number Generator",                                */
-/* ACM Transactions on Modeling and Computer Simulation,           */
-/* Vol. 8, No. 1, January 1998, pp 3--30.                          */
-
-// See http://www.math.keio.ac.jp/~matumoto/emt.html
-// And http://www.acm.org/pubs/citations/journals/tomacs/1998-8-1/p3-matsumoto/
-
-// 1999-01-25 adapted to STL-like idiom
-// allan@stokes.ca (Allan Stokes) www.stokes.ca
 
 /*
  * $Id$
  *
- * $Log$
- * Revision 1.3  2002/03/07 14:37:19  patricg
+ * A C-program for MT19937: Integer version (1998/4/6)            
+ *  genrand() generates one pseudorandom unsigned integer (32bit) 
+ * which is uniformly distributed among 0 to 2^32-1  for each    
+ * call. sgenrand(seed) set initial values to the working area   
+ * of 624 words. Before genrand(), sgenrand(seed) must be        
+ * called once. (seed is any 32-bit integer except for 0).
+ *   Coded by Takuji Nishimura, considering the suggestions by    
+ * Topher Cooper and Marc Rieffel in July-Aug. 1997.             
  *
- * fixed use of STL iterator
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Library General Public   
+ * License as published by the Free Software Foundation; either    
+ * version 2 of the License, or (at your option) any later         
+ * version.                                                        
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.            
+ * See the GNU Library General Public License for more details.    
+ * You should have received a copy of the GNU Library General      
+ * Public License along with this library; if not, write to the    
+ * Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA    
+ * 02111-1307  USA                                                 
  *
- * Revision 1.2  2001/01/26 19:52:34  tveldhui
- * Incorporated changes from Max Domeika for STL compatibility.
+ * Copyright (C) 1997 Makoto Matsumoto and Takuji Nishimura.       
+ * When you use this, send an email to: matumoto@math.keio.ac.jp   
+ * with an appropriate reference to your work.                     
+ *
+ * REFERENCE                                                       
+ * M. Matsumoto and T. Nishimura,                                  
+ * "Mersenne Twister: A 623-Dimensionally Equidistributed Uniform
+ * Pseudo-Random Number Generator",                                
+ * ACM Transactions on Modeling and Computer Simulation,           
+ * Vol. 8, No. 1, January 1998, pp 3--30.                          
+ *
+ * See 
+ *     http://www.math.keio.ac.jp/~matumoto/emt.html
+ * and
+ *     http://www.acm.org/pubs/citations/journals/tomacs/1998-8-1/p3-matsumoto/
  *
  */
 
