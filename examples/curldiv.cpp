9d0ec5a95782336f3ab824599ea09237fd6f2546
julianc <devnull@localhost>
2003-02-20 21:20:38 +0000

    Made some repairs to this example code so that it properly uses stencil ops.

--- examples/curldiv.cpp
+++ examples/curldiv.cpp
@@ -1,23 +1,26 @@
 // Div, grad, curl and all that
 
 #include <blitz/array.h>
+#include <blitz/tinyvec.h>
 
 BZ_USING_NAMESPACE(blitz)
 BZ_USING_NAMESPACE(blitz::tensor)
 
-BZ_DECLARE_STENCIL6(test,Ax,Ay,Az,R1,R2,R3)
-    TinyVector<float,3> curlA = curl(Ax,Ay,Az);
-    float divA = div(Ax,Ay,Az);
-    TinyVector<float,3> gradAx = grad3D(Ax);
-    TinyVector<float,3> gradAy = grad3D(Ay);
-    TinyVector<float,3> gradAz = grad3D(Az);
+BZ_DECLARE_STENCIL8(test,Ax,Ay,Az,curlA,divA,gradAx,gradAy,gradAz)
+    curlA = curl(Ax,Ay,Az);
+    divA = div(Ax,Ay,Az);
+    gradAx = grad3D(Ax);
+    gradAy = grad3D(Ay);
+    gradAz = grad3D(Az);
 BZ_STENCIL_END
 
 int main()
 {
-    Array<float,3> Ax, Ay, Az, R1, R2, R3;
+    Array<float,3> Ax, Ay, Az, divA;
+    Array<TinyVector<float,3>,3> gradAx, gradAy, gradAz, curlA;
     const int N = 40;
-    allocateArrays(shape(N,N,N), Ax, Ay, Az, R1, R2, R3);
+    allocateArrays(shape(N,N,N), Ax, Ay, Az, divA);
+    allocateArrays(shape(N,N,N), gradAx, gradAy, gradAz, curlA);
 
     Array<float,1> cx(N), cy(N), cz(N);
     float h = 1.0 / (N-1);
@@ -30,6 +33,6 @@ int main()
     Ay = sin(cx(i)) + sin(cy(j)) + sin(cz(k));
     Az = exp(cx(i)) + exp(cy(j)) + exp(cz(k));
 
-    applyStencil(test(), Ax, Ay, Az, R1, R2, R3);
+    applyStencil(test(), Ax, Ay, Az, curlA, divA, gradAx, gradAy, gradAz);
 }
 
