23bab723f642fcde9e86348a19a096a52f928b06
julianc <devnull@localhost>
2003-11-11 21:05:37 +0000

    Removed the superfluous combination of ETBase<T1> and T2.  If the first operand in a binary function or operator is not an Array, ArrayExpr or IndexPlaceholder, it is assumed to be a POD type.  The combination of T1 and ETBase<T2> handles all combinations of a POD type with a Blitz Array or Array-like type.

--- blitz/array/newet-macros-old.h
+++ blitz/array/newet-macros-old.h
@@ -58,7 +58,6 @@ name(const ETBase<T1>& d1)                                                \
  */
 
 #define BZ_DECLARE_ARRAY_ET_BINARY(name, applic)                          \
-                                                                          \
 template<class T_numtype1, int N_rank1, class T_other>                    \
 _bz_inline_et                                                             \
 _bz_ArrayExpr<_bz_ArrayExprOp<FastArrayIterator<T_numtype1, N_rank1>,     \
@@ -87,19 +86,17 @@ name(const _bz_ArrayExpr<T_expr1>& d1, const T_other& d2)                 \
         _bz_typename asExpr<T_other>::T_expr::T_numtype> > >(d1,d2);      \
 }                                                                         \
                                                                           \
-template<class T1, class T2>                                              \
+template<int N1, class T_other>                                           \
 _bz_inline_et                                                             \
-_bz_ArrayExpr<_bz_ArrayExprOp<_bz_typename asExpr<T1>::T_expr,            \
-    _bz_typename asExpr<T2>::T_expr,                                      \
-    applic<_bz_typename asExpr<T1>::T_expr::T_numtype,                    \
-    _bz_typename asExpr<T2>::T_expr::T_numtype> > >                       \
-name(const ETBase<T1>& d1, const T2& d2)                                  \
+_bz_ArrayExpr<_bz_ArrayExprOp<IndexPlaceholder<N1>,                       \
+    _bz_typename asExpr<T_other>::T_expr,                                 \
+    applic<int, _bz_typename asExpr<T_other>::T_expr::T_numtype> > >      \
+name(IndexPlaceholder<N1> d1, const T_other& d2)                          \
 {                                                                         \
-    return _bz_ArrayExpr<_bz_ArrayExprOp<_bz_typename asExpr<T1>::T_expr, \
-        _bz_typename asExpr<T2>::T_expr,                                  \
-        applic<_bz_typename asExpr<T1>::T_expr::T_numtype,                \
-        _bz_typename asExpr<T2>::T_expr::T_numtype> > >                   \
-        (static_cast<const T1&>(d1), d2);                                 \
+    return _bz_ArrayExpr<_bz_ArrayExprOp<IndexPlaceholder<N1>,            \
+        _bz_typename asExpr<T_other>::T_expr,                             \
+        applic<int, _bz_typename asExpr<T_other>::T_expr::T_numtype> > >  \
+        (d1,d2);                                                          \
 }                                                                         \
                                                                           \
 template<class T1, class T2>                                              \
@@ -115,19 +112,6 @@ name(const T1& d1, const ETBase<T2>& d2)                                  \
         applic<_bz_typename asExpr<T1>::T_expr::T_numtype,                \
         _bz_typename asExpr<T2>::T_expr::T_numtype> > >                   \
         (d1, static_cast<const T2&>(d2));                                 \
-}                                                                         \
-                                                                          \
-template<int N1, class T_other>                                           \
-_bz_inline_et                                                             \
-_bz_ArrayExpr<_bz_ArrayExprOp<IndexPlaceholder<N1>,                       \
-    _bz_typename asExpr<T_other>::T_expr,                                 \
-    applic<int, _bz_typename asExpr<T_other>::T_expr::T_numtype> > >      \
-name(IndexPlaceholder<N1> d1, const T_other& d2)                          \
-{                                                                         \
-    return _bz_ArrayExpr<_bz_ArrayExprOp<IndexPlaceholder<N1>,            \
-        _bz_typename asExpr<T_other>::T_expr,                             \
-        applic<int, _bz_typename asExpr<T_other>::T_expr::T_numtype> > >  \
-        (d1,d2);                                                          \
 }
 
 /*
