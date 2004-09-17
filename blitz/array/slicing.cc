fa1aa11c3ebb75ba8412c72f9e7b68ecfd492451
julianc <devnull@localhost>
2004-09-17 08:38:44 +0000

    Use default value of zero for second argument in call to changeBlock method of class MemoryBlockReference.  No need to set data_ pointer to array.dataZero(), as this is already done by changeBlock.

--- blitz/array/slicing.cc
+++ blitz/array/slicing.cc
@@ -214,8 +214,7 @@ void Array<P_numtype, N_rank>::constructSlice(Array<T_numtype, N_rank2>& array,
     R0 r0, R1 r1, R2 r2, R3 r3, R4 r4, R5 r5, R6 r6, R7 r7, R8 r8, R9 r9,
     R10 r10)
 {
-    MemoryBlockReference<T_numtype>::changeBlock(array, array.zeroOffset());
-    data_ = array.dataZero();
+    MemoryBlockReference<T_numtype>::changeBlock(array);
 
     int setRank = 0;
 
