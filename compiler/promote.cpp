80e5f808ff2fdb3a3d22b78c7a4eb12073548ecb
julianc <devnull@localhost>
2004-10-06 05:33:24 +0000

    This file contained an empty program, so I wrote an actual test for the vector type promotion mechanism using traits, as described in "Using C++ Traits Classes for Scientific Computing" by Todd Veldhuizen (1996).

--- compiler/promote.cpp
+++ compiler/promote.cpp
@@ -1,1 +1,30 @@
-int main() { return 0; }
+#ifndef HAVE_TYPENAME
+ #define typename
+#endif
+
+template <class T> 
+struct vec3 { T data_[3]; };
+
+template <class T1, class T2> 
+struct promote_trait { typedef T1 T_promote; };
+template <> 
+struct promote_trait<int,double> { typedef double T_promote; };
+
+template <class T1, class T2> 
+vec3<typename promote_trait<T1,T2>::T_promote>
+operator+(const vec3<T1>& a, const vec3<T2>& b) { 
+  vec3<typename promote_trait<T1,T2>::T_promote> c;
+  c.data_[0] = a.data_[0] + b.data_[0];
+  c.data_[1] = a.data_[1] + b.data_[1];
+  c.data_[2] = a.data_[2] + b.data_[2]; 
+  return c; 
+}
+
+int main() { 
+  vec3<int> a,b; 
+  vec3<double> c,d,e; 
+  b=a+a; 
+  d=c+c; 
+  e=b+d; 
+  return 0; 
+}
