e7bc403cc32eae195129827f0da95262cb24d10c
julianc <devnull@localhost>
2004-10-06 20:30:10 +0000

    Use new master file <blitz/bzconfig.h> to include a compiler-specific config file.  Use new version of preprocessor symbols defined by autoconf macros.

--- blitz/compiler.h
+++ blitz/compiler.h
@@ -25,35 +25,36 @@
 #ifndef BZ_COMPILER_H
 #define BZ_COMPILER_H
 
-// The file <blitz/config.h> is generated automatically by configure.
+// The file <blitz/bzconfig.h> is used to select a compiler-specific
+// config.h file that is generated automatically by configure.
 
-#include <blitz/config.h>
+#include <blitz/bzconfig.h>
 
 /*
  * Define some kludges.
  */
 
-#ifndef BZ_TEMPLATES
+#ifndef BZ_HAVE_TEMPLATES
     #error  In <blitz/config.h>: A working template implementation is required by Blitz++ (you may need to rerun the compiler/bzconfig script)
 #endif
 
-#ifndef BZ_MEMBER_TEMPLATES
+#ifndef BZ_HAVE_MEMBER_TEMPLATES
   #error  In <blitz/config.h>: Your compiler does not support member templates.  (you may need to rerun the compiler/bzconfig script)
 #endif
 
-#ifndef BZ_FULL_SPECIALIZATION_SYNTAX
+#ifndef BZ_HAVE_FULL_SPECIALIZATION_SYNTAX
   #error In <blitz/config.h>: Your compiler does not support template<> full specialization syntax.  You may need to rerun the compiler/bzconfig script.
 #endif
 
-#ifndef BZ_PARTIAL_ORDERING
+#ifndef BZ_HAVE_PARTIAL_ORDERING
   #error In <blitz/config.h>: Your compiler does not support partial ordering (you may need to rerun the compiler/bzconfig script)
 #endif
 
-#ifndef BZ_PARTIAL_SPECIALIZATION
+#ifndef BZ_HAVE_PARTIAL_SPECIALIZATION
   #error In <blitz/config.h>: Your compiler does not support partial specialization (you may need to rerun the compiler/bzconfig script)
 #endif
 
-#ifdef BZ_NAMESPACES
+#ifdef BZ_HAVE_NAMESPACES
     #define BZ_NAMESPACE(X)        namespace X {
     #define BZ_NAMESPACE_END       }
     #define BZ_USING_NAMESPACE(X)  using namespace X;
@@ -63,51 +64,51 @@
     #define BZ_USING_NAMESPACE(X)
 #endif
 
-#ifdef BZ_TEMPLATE_QUALIFIED_RETURN_TYPE
+#ifdef BZ_HAVE_TEMPLATE_QUALIFIED_RETURN_TYPE
   #define BZ_USE_NUMTRAIT
 #endif
 
-#ifdef BZ_DEFAULT_TEMPLATE_PARAMETERS
+#ifdef BZ_HAVE_DEFAULT_TEMPLATE_PARAMETERS
     #define BZ_TEMPLATE_DEFAULT(X)   = X
 #else
     #define BZ_TEMPLATE_DEFAULT
 #endif
 
-#ifndef BZ_EXPLICIT
+#ifndef BZ_HAVE_EXPLICIT
     #define explicit   
 #endif
 
-#ifdef BZ_TYPENAME
+#ifdef BZ_HAVE_TYPENAME
     #define _bz_typename     typename
 #else
     #define _bz_typename
 #endif
 
-#ifndef BZ_MUTABLE
+#ifndef BZ_HAVE_MUTABLE
     #define mutable
 #endif
 
 #ifdef BZ_DISABLE_RESTRICT
- #undef BZ_NCEG_RESTRICT
+ #undef BZ_HAVE_NCEG_RESTRICT
 #endif
 
-#ifndef BZ_NCEG_RESTRICT
-    #if defined(BZ_NCEG_RESTRICT_EGCS)
+#ifndef BZ_HAVE_NCEG_RESTRICT
+    #if defined(BZ_HAVE_NCEG_RESTRICT_EGCS)
         #define restrict     __restrict__
     #else
         #define restrict
     #endif
 #endif
 
-#if !defined(BZ_BOOL) && !defined(BZ_NO_BOOL_KLUDGE)
+#if !defined(BZ_HAVE_BOOL) && !defined(BZ_NO_BOOL_KLUDGE)
     #define bool    int
     #define true    1
     #define false   0
 #endif
 
-#ifdef BZ_ENUM_COMPUTATIONS_WITH_CAST
+#ifdef BZ_HAVE_ENUM_COMPUTATIONS_WITH_CAST
     #define BZ_ENUM_CAST(X)   (int)X
-#elif defined(BZ_ENUM_COMPUTATIONS)
+#elif defined(BZ_HAVE_ENUM_COMPUTATIONS)
     #define BZ_ENUM_CAST(X)   X
 #else
     #error In <blitz/config.h>: Your compiler does not support enum computations.  You may have to rerun compiler/bzconfig.
@@ -115,33 +116,33 @@
 
 #if defined(BZ_MATH_FN_IN_NAMESPACE_STD)
   #define BZ_MATHFN_SCOPE(x) std::x
-#elif defined(BZ_NAMESPACES)
+#elif defined(BZ_HAVE_NAMESPACES)
   #define BZ_MATHFN_SCOPE(x) ::x
 #else
   #define BZ_MATHFN_SCOPE(x) x
 #endif
 
-#if defined(BZ_COMPLEX_MATH_IN_NAMESPACE_STD)
+#if defined(BZ_HAVE_COMPLEX_MATH_IN_NAMESPACE_STD)
   #define BZ_CMATHFN_SCOPE(x) std::x
-#elif defined(BZ_NAMESPACES)
+#elif defined(BZ_HAVE_NAMESPACES)
   #define BZ_CMATHFN_SCOPE(x) ::x
 #else
   #define BZ_CMATHFN_SCOPE(x) x
 #endif
 
-#if defined(BZ_NAMESPACES)
+#if defined(BZ_HAVE_NAMESPACES)
   #define BZ_IEEEMATHFN_SCOPE(x) ::x
 #else
   #define BZ_IEEEMATHFN_SCOPE(x) x
 #endif
 
-#if defined(BZ_NAMESPACES)
+#if defined(BZ_HAVE_NAMESPACES)
   #define BZ_BLITZ_SCOPE(x) blitz::x
 #else
   #define BZ_BLITZ_SCOPE(x) ::x
 #endif
 
-#if defined(BZ_NAMESPACES)
+#if defined(BZ_HAVE_NAMESPACES) && defined(BZ_HAVE_STD)
   #define BZ_STD_SCOPE(x) std::x
 #else
   #define BZ_STD_SCOPE(x) ::x
