9ea1dcddc6b74bdd29b7c0786e22292dc6a773f0
julianc <devnull@localhost>
2005-10-07 17:04:41 +0000

    Modify include paths to have the blitz/ prefix.  Use angled brackets instead of quotes around header file name.  Added error message if compiler unknown.

--- blitz/bzconfig.h
+++ blitz/bzconfig.h
@@ -4,53 +4,56 @@
 
 #if defined(__APPLE)
 /* IBM xlc compiler for Darwin */
-#include "apple/bzconfig.h"
+#include <blitz/apple/bzconfig.h>
 
 #elif defined(__ICC)
 /* Intel icc compiler */
-#include "intel/bzconfig.h"
+#include <blitz/intel/bzconfig.h>
 
 #elif defined(_MSC_VER)
 /* Microsoft VS.NET compiler */
-#include "ms/bzconfig.h"
+#include <blitz/ms/bzconfig.h>
 
 #elif defined(__IBM)
 /* IBM xlC compiler */
-#include "ibm/bzconfig.h"
+#include <blitz/ibm/bzconfig.h>
 
 #elif defined(__DECCXX)
 /* Compaq cxx compiler */
-#include "compaq/bzconfig.h"
+#include <blitz/compaq/bzconfig.h>
 
 #elif defined(__HP_aCC)
 /* HP aCC compiler */
-#include "hp/bzconfig.h"
+#include <blitz/hp/bzconfig.h>
 
 #elif defined(_SGI_COMPILER_VERSION)
 /* SGI CC compiler */
-#include "sgi/bzconfig.h"
+#include <blitz/sgi/bzconfig.h>
 
 #elif defined(__SUNPRO_CC)
 /* SunPRO CC compiler */
-#include "sun/bzconfig.h"
+#include <blitz/sun/bzconfig.h>
 
 #elif defined(__GNUC__)
 /* GNU gcc compiler */
-#include "gnu/bzconfig.h"
+#include <blitz/gnu/bzconfig.h>
 
 #elif defined(__PGI)
 /* PGI pgCC compiler */
-#include "pgi/bzconfig.h"
+#include <blitz/pgi/bzconfig.h>
 
 #elif defined(__KCC)
 /* KAI KCC compiler */
-#include "kai/bzconfig.h"
+#include <blitz/kai/bzconfig.h>
 
 #elif defined(__FUJITSU)
 /* Fujitsu FCC compiler */
-#include "fujitsu/bzconfig.h"
+#include <blitz/fujitsu/bzconfig.h>
 
 /* Add other compilers here */
+
+#else
+#error Unknown compiler
 #endif
 
 #endif /* BZCONFIG_H */
