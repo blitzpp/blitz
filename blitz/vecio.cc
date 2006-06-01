2f99b8e06cc244cbadbffb7fd9367c7f25bbbeaf
julianc <devnull@localhost>
2006-06-01 01:20:41 +0000

    Make Vector output format similar to 1D Array.  We write out the Vector length, followed by a space-delimited list of elements enclosed in brackets.  Added a compatible input operator.

--- blitz/vecio.cc
+++ blitz/vecio.cc
@@ -23,14 +23,12 @@ BZ_NAMESPACE(blitz)
 template<typename P_numtype>
 ostream& operator<<(ostream& os, const Vector<P_numtype>& x)
 {
-    os << "[ ";
+    os << x.length() << endl << "[ ";
     for (int i=0; i < x.length(); ++i)
     {
-        os << setw(10) << x[i];
-        if (!((i+1)%7))
-            os << endl << "  ";
+        os << x[i] << " ";
     }
-    os << " ]";
+    os << "]";
     return os;
 }
 
@@ -40,6 +38,30 @@ ostream& operator<<(ostream& os, _bz_VecExpr<P_expr> expr)
     Vector<_bz_typename P_expr::T_numtype> result(expr);
     os << result;
     return os;
+}
+
+template<typename P_numtype>
+istream& operator>>(istream& is, Vector<P_numtype>& x)
+{
+    int length;
+    char sep;
+
+    is >> length;
+    BZPRECHECK(!is.bad(), "Premature end of input while scanning Vector");
+    x.resize(length);
+
+    is >> sep;
+    BZPRECHECK(sep == '[', "Format error while scanning input \
+Vector \n (expected '[' before beginning of Vector data)");
+    for (int i=0; i < x.length(); ++i) {
+        BZPRECHECK(!is.bad(), "Premature end of input while scanning Vector");
+        is >> x(i);
+    }
+
+    is >> sep;
+    BZPRECHECK(sep == ']', "Format error while scanning input \
+Vector \n (expected ']' after end of Vector data)");
+    return is;
 }
 
 BZ_NAMESPACE_END
