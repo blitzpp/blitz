d0b076a8fcab4e0938156dc38890daa4de849463
patricg <devnull@localhost>
2011-02-14 12:47:29 +0000

    Small utility script to update ChangeLog by calling `cvs2cl' if found. Currently the date filter everything strictly after 2009-12-31.

---
+++ bin/update-cl.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Script to update ChangeLog file using the utility
+# command cvs2cl if found on the current system
+
+which cvs2cl &> /dev/null || exit 1
+
+userfile=`mktemp`
+
+echo creating temporary usermap $userfile
+
+cat >> $userfile << EOF
+patrick:'Patrick Guio <p.guio@ucl.ac.uk>'
+patricg:'Patrick Guio <p.guio@ucl.ac.uk>'
+patrickg:'Patrick Guio <patrick.guio@matnat.uio.no>'
+pguio:'Patrick Guio <patrick.guio@matnat.uio.no>'
+julianc:'Julian Cummings  <cummings@cacr.caltech.edu>'
+papadop:'Theodore Papadopoulo <Theodore.Papadopoulo@sophia.inria.fr>'
+grifgrif:'Patrik Jonsson <code@familjenjonsson.org>'
+EOF
+
+
+cvs2cl --prune --no-times --no-common-dir \
+	-f ChangeLog -U $userfile -l "-d>2009-12-31"
+
+echo removing temporary usermap $userfile
+rm -rf $userfile
