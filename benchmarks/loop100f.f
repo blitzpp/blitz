47f5f8ae2733212c754e7567b1c3c3281bed627b
Patrik Jonsson <grifgrif@users.sourceforge.net>
2011-06-16 15:02:19 -0700

    Added loop100 fortran files.

---
+++ benchmarks/loop100f.f
@@ -0,0 +1,18 @@
+
+      SUBROUTINE loop100_F77(N, a, b, c, d, x, u, v, w, z)
+      INTEGER i, N
+      REAL*8 a(N), b(N), c(N), d(N), x(N), u, v, w, z
+
+      DO i=1,N
+          x(i)=(1.0-c(i)*c(i))/((4*w)*sin(1.0+c(i)*c(i)-2*v*c(i)))*a(i
+     !)*b(i)*u*exp(-z*d(i))
+      END DO
+      RETURN
+      END
+
+
+      SUBROUTINE loop100_F77Overhead(N, a, b, c, d, x, u, v, w, z)
+      INTEGER i, N
+      REAL*8 a(N), b(N), c(N), d(N), x(N), u, v, w, z
+      RETURN
+      END
---
+++ benchmarks/loop100f90.f90
@@ -0,0 +1,16 @@
+
+      SUBROUTINE loop100_F90(N, x, y, a)
+      INTEGER i, N
+      REAL*8 a(N), b(N), c(N), d(N), x(N), u, v, w, z
+
+      x=(1.0-c*c)/((4*w)*sin(1.0+c*c-2*v*c))*a*b*u*exp(-z*d)
+      RETURN
+      END
+
+
+      SUBROUTINE loop100_F90Overhead(N, x, y, a)
+      INTEGER i, N
+      REAL*8 a(N), b(N), c(N), d(N), x(N), u, v, w, z
+
+      RETURN
+      END
