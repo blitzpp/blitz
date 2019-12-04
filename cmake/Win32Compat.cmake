b5aef532c509940aca8093f3966ceb3c8e15de39
David Blankenship <david.blankenship@kla-tencor.com>
2019-12-04 09:26:36 -0600

    Windows build
    Changes for building on Windows with MSVC 2019.

--- cmake/Win32Compat.cmake
+++ cmake/Win32Compat.cmake
@@ -3,7 +3,7 @@
 # ==============================
 
 IF (WIN32)
-    SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${OpenMEEG_BINARY_DIR}/src")
+    SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/src")
 
     #   These are obsolete as per cmake 2.6
 
