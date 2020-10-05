d7dd82fbcf007954d02b37efd35f94cea59d2350
Dr. András Vukics <andras.vukics@gmail.com>
2020-10-05 20:59:30 +0200

    Adopted “modern cmake” – configuration now works and compilation also goes a long way

--- blitz/blitz.h
+++ blitz/blitz.h
@@ -49,7 +49,7 @@
  #endif
 #endif
 
-#include <blitz/bzconfig.h>
+#include <blitz/config.h>
 #include <blitz/compiler.h>          // Compiler-specific directives
 #include <blitz/tuning.h>            // Performance tuning
 #include <blitz/tau.h>               // Profiling
